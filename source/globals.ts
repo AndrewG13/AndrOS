2c1219684b0815b87ba4853de30c0ad67aea0b24
AndrewG13 <andrew.giardina1@marist.edu>
2021-08-31 17:01:51 -0400

    Changed the 'Ver' command. ISSUE #1

--- source/globals.ts
+++ source/globals.ts
@@ -11,8 +11,10 @@
 //
 // Global CONSTANTS (TypeScript 1.5 introduced const. Very cool.)
 //
-const APP_NAME: string    = "TSOS";   // 'cause Bob and I were at a loss for a better name.
-const APP_VERSION: string = "0.07";   // What did you expect?
+const APP_NAME: string    = "AndrOS";   // The name just came to me, ya know?
+const APP_VERSION: string = "64";     // I'm trying to make you laugh like as if all thousands
+ 																				// of students you've taught haven't done the same.
+																				// ... tough competition ...
 
 const CPU_CLOCK_INTERVAL: number = 100;   // This is in ms (milliseconds) so 1000 = 1 second.
 
@@ -42,11 +44,11 @@ var _Trace: boolean = true;              // Default the OS trace to be on.
 // The OS Kernel and its queues.
 var _Kernel: TSOS.Kernel;
 var _KernelInterruptQueue: TSOS.Queue = null;
-var _KernelInputQueue: TSOS.Queue = null; 
-var _KernelBuffers = null; 
+var _KernelInputQueue: TSOS.Queue = null;
+var _KernelBuffers = null;
 
 // Standard input and output
-var _StdIn:  TSOS.Console = null; 
+var _StdIn:  TSOS.Console = null;
 var _StdOut: TSOS.Console = null;
 
 // UI
