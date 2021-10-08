3a57b2622259ae4ad062f59400ab5fce12369dcf
AndrewG13 <andrew.giardina1@marist.edu>
2021-10-07 21:00:46 -0400

    PCB concept ISSUE#14

---
+++ source/os/processControlBlock.ts
@@ -0,0 +1,22 @@
+/* ------------
+     ProcessControlBlock.ts
+
+
+     ------------ */
+
+     module TSOS {
+
+        export class PCB {
+            //public variable : number;
+            
+            // According to notes, should have:
+            // State, PID, PC, X Y regs, Z flag, knowledge of memory "block" / range 
+
+            constructor() {
+                //this.variable = 0;
+            }
+            
+            
+        }
+    }
+    
