a0a6f36733459f9e818165a36d434457382cef89
Andrew Giardina <47252267+AndrewG13@users.noreply.github.com>
2021-12-12 17:20:47 -0500

    Fixes to DiskDriver

--- source/os/deviceDriverDisk.ts
+++ source/os/deviceDriverDisk.ts
@@ -1,5 +1,5 @@
 /* ----------------------------------
-   DeviceDriverKeyboard.ts
+   DeviceDriverDisk.ts
 
    The Kernel Disk Device Driver.
    ---------------------------------- */
@@ -9,18 +9,13 @@
     // Extends DeviceDriver
     export class DeviceDriverDisk extends DeviceDriver {
 
-        //private numSyms : number[] = [41,33,64,35,36,37,94,38,42,40];
-
         constructor() {
             // Override the base method pointers.
-
-            // The code below cannot run because "this" can only be
-            // accessed after calling super.
-            // super(this.krnKbdDriverEntry, this.krnKbdDispatchKeyPress);
-            // So instead...
+            // MyTODO: look into what the hell ^ means...
+           
             super();
             this.driverEntry = this.krnDskDriverEntry;
-            //this.isr = this.krnKbdDispatchKeyPress;
+            //this.isr = ???
         }
 
         public krnDiskStatus() : boolean {
@@ -28,9 +23,8 @@
         }
 
         public krnDskDriverEntry() {
-            // Initialization routine for this, the kernel-mode Keyboard Device Driver.
+            // Initialization routine for this, the kernel-mode Disk Device Driver.
             this.status = "loaded";
-            // More?
         }
 
         public krnDskCreateRtn() {
